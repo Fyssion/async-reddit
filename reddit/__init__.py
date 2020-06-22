34b7c01db939ae62eb6caefd61e78bd95f92844d
Fyssion <fyssioncodes@gmail.com>
2020-06-22 17:39:05 -0500

    Add version

--- reddit/__init__.py
+++ reddit/__init__.py
@@ -22,6 +22,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+__version__ = "0.1.0a"
+
 from .client import Client
 from .objects import Subreddit, Redditor
 from .errors import *
