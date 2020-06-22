d702a6ea5ef48746b4ff97774f6e816ec68d6170
Fyssion <fyssioncodes@gmail.com>
2020-06-22 17:58:18 -0500

    Bump version

--- reddit/__init__.py
+++ reddit/__init__.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-__version__ = "0.1.0a"
+__version__ = "0.1.0"
 
 from .client import Client
 from .objects import Subreddit, Redditor
