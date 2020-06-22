0fb3858963853dea322efb75db8987815101f381
Fyssion <fyssioncodes@gmail.com>
2020-06-22 17:58:11 -0500

    Fix spacing

--- reddit/errors.py
+++ reddit/errors.py
@@ -22,6 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+
 # Base exception that all exceptions subclass
 class RedditException(Exception):
     pass
