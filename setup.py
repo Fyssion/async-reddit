7311af62d5474039db49bcd03e72bf3c4bda8513
Fyssion <fyssioncodes@gmail.com>
2020-06-22 17:39:17 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,59 @@
+import setuptools
+import re
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+version = ''
+with open('reddit/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('version is not set')
+
+if version.endswith(('a', 'b', 'rc')):
+    # append version identifier based on commit count
+    try:
+        import subprocess
+        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += out.decode('utf-8').strip()
+        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += '+g' + out.decode('utf-8').strip()
+    except Exception:
+        pass
+
+
+setuptools.setup(
+    name="async-reddit",
+    version=version,
+    author="Fyssion",
+    author_email="fyssioncodes@gmail.com",
+    description="An async API wrapper for read-only Reddit written in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Fyssion/async-reddit",
+    project_urls={
+        "Documentation": "https://www.fyssioncodes.com/async-reddit/",
+        "Issue tracker": "https://github.com/Fyssion/async-reddit/issues",
+      },
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
