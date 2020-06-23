b92075c4223113535414f486a24f915d826f20d9
Fyssion <fyssioncodes@gmail.com>
2020-06-22 20:00:56 -0500

    Change master-doc

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,9 @@ intersphinx_mapping = {
   'aio': ('https://aiohttp.readthedocs.io/en/stable/', None),
 }
 
+# The master toctree document.
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
