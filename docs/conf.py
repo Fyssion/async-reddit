6d2c129476fcfdcbeb44f38f2137569c4ba42847
Fyssion <fyssioncodes@gmail.com>
2020-06-22 19:55:03 -0500

    Add an extension

--- docs/conf.py
+++ docs/conf.py
@@ -38,8 +38,14 @@ release = version
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.autodoc"
+    "sphinx.ext.autodoc",
+    'sphinx.ext.napoleon',
 ]
+
+intersphinx_mapping = {
+  'py': ('https://docs.python.org/3', None),
+  'aio': ('https://aiohttp.readthedocs.io/en/stable/', None),
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
